1cf188c4ca9af0a42f8eee315665a691c3d1bf95
Hiy <judetadeja17@gmail.com>
2025-04-30 22:23:07 +0800

    merge/modified version

---
+++ js/tutorial.js
@@ -0,0 +1,7 @@
+function toggleDropdown(header) {
+    const content = header.nextElementSibling;
+    const arrow = header.querySelector(".arrow");
+    content.classList.toggle("active");
+    arrow.classList.toggle("active");
+  }
+  

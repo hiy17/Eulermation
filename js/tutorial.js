2c29f941dca4ab07a4316ab3c17be275615cf91a
JT <91821132+hiy17@users.noreply.github.com>
2025-05-01 17:46:02 +0800

    Delete js directory

--- js/tutorial.js
+++
@@ -1,7 +0,0 @@
-function toggleDropdown(header) {
-    const content = header.nextElementSibling;
-    const arrow = header.querySelector(".arrow");
-    content.classList.toggle("active");
-    arrow.classList.toggle("active");
-  }
-  
